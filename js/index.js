2cc5c547cfc870705af71753d74f54e7435ddd51
Kaique Ronie <kaiqueronie3050@gmail.com>
2024-03-15 21:12:24 -0300

    site finalizado

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 function enviar(){
-    alert('cliquei')
+    let nome = document.getElementById("nome").value;
+    let money = document.getElementById('money').value;
+    var msg = 'Olá ' + nome + 'Obrigado por ter doado o valor R$:' + money + ' A org Cão sem dono agradeçe';
+    alert(msg);
 }
